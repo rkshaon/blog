41946e03169ab11d0304f1d64805de8d17b6431b
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-05-25 20:38:10 +0600

    backend: user - model defined

--- backend/user_api/admin.py
+++ backend/user_api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from user_api.models import User
+
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'is_active', 'is_staff')
+    list_filter = ('is_active', 'is_staff')
+    search_fields = ('username', 'email')
+    readonly_fields = ('id',)
--- backend/user_api/models.py
+++ backend/user_api/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        user = self.create_user(email, password, **extra_fields)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class User(AbstractUser, PermissionsMixin):
+    ROLE_CHOICES = (
+        ('blogger', 'Blogger'),
+        ('moderator', 'Moderator'),
+        ('admin', 'Admin'),
+    )
+
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    role = models.CharField(
+        max_length=20, choices=ROLE_CHOICES, default='blogger')
+    # is_active = models.BooleanField(default=True)
+    # is_staff = models.BooleanField(default=False)
+    added_date_time = models.DateTimeField(auto_now_add=True)
+    updated_date_time = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'role']
+
+
+    def __str__(self):
+        return self.email
