0c769a79c6931d4688a90d51adb135e2fe848450
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-05-27 23:40:54 +0600

    backend: blog - post api - done

--- backend/blog_api/models.py
+++ backend/blog_api/models.py
@@ -8,13 +8,21 @@ class Blog(models.Model):
         ('published', 'Published'),
     )
 
-    author = models.ForeignKey('user_api.User', on_delete=models.CASCADE, related_name='author')
+    author = models.ForeignKey(
+        'user_api.User', 
+        on_delete=models.CASCADE, 
+        related_name='author')
     title = models.CharField(max_length=255)
     content = models.TextField()
     
     blog_status = models.CharField(max_length=20,choices=BLOG_STATUS_CHOICES, default='draft')
     is_approved = models.BooleanField(default=False)
-    approved_by = models.ForeignKey('user_api.User', on_delete=models.CASCADE, blank=True, null=True, related_name='approver')
+    approved_by = models.ForeignKey(
+        'user_api.User', 
+        on_delete=models.CASCADE, 
+        blank=True, 
+        null=True, 
+        related_name='approver')
 
     added_date_time = models.DateTimeField(auto_now_add=True)
     updated_date_time = models.DateTimeField(auto_now=True)
--- backend/blog_api/serializers.py
+++ backend/blog_api/serializers.py
@@ -6,10 +6,16 @@ from user_api.serializers import UserSerializer
 
 
 
-class BlogSerializer(serializers.ModelSerializer):
-    author = UserSerializer()
-    approved_by = UserSerializer()
-    
+class BlogSerializer(serializers.ModelSerializer):    
     class Meta:
         fields = '__all__'
         model = Blog
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['author'] = UserSerializer(instance.author).data
+
+        if instance.approved_by:
+            representation['approved_by'] = UserSerializer(instance.approved_by).data
+        
+        return representation
