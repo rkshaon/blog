44419eaa952dea1231c723f0427925a63320fedd
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-05-27 17:51:29 +0600

    backend: blog - app installed, blog model created

--- backend/blog/settings.py
+++ backend/blog/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'user_api',
+    'blog_api',
 ]
 
 MIDDLEWARE = [
---
+++ backend/blog_api/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+
+class Blog(models.Model):
+    BLOG_STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    author = models.ForeignKey('user_api.User', on_delete=models.CASCADE, related_name='author')
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    
+    blog_status = models.CharField(max_length=20,choices=BLOG_STATUS_CHOICES, default='draft')
+    is_approved = models.BooleanField(default=False)
+    approved_by = models.ForeignKey('user_api.User', on_delete=models.CASCADE, blank=True, null=True, related_name='approver')
+
+    added_date_time = models.DateTimeField(auto_now_add=True)
+    updated_date_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}: {self.author.username}"
