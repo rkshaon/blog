a022b7c4cbe41bc587c54372c9c548e2e7a22d3e
HussainShafayet <shafayetsec09@gmail.com>
2024-06-01 00:39:48 +0600

    Tailwind css Installation This commit will be tailwind css Installation and first use

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <>
-     Welcome to our blog
+     <div className="flex items-center justify-center h-screen bg-gray-200">
+      <h1 className="text-4xl font-bold text-blue-500"> Welcome to our blog!</h1>
+    </div>
     </>
   );
 }
