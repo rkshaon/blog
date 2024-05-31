871579311f2e9a5e5a73c4809eb64747d83b93f9
HussainShafayet <shafayetsec09@gmail.com>
2024-06-01 03:19:15 +0600

    (Feat) Navber Design This commit will be create responisve navber

--- frontend_react/src/components/Layout.js
+++ frontend_react/src/components/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 const Layout = ({ children }) => (
   <div className="min-h-screen grid grid-rows-[auto_1fr_auto] grid-cols-1 md:grid-cols-3 md:grid-rows-[repeat(12,_min-content)] gap-2 p-2">
     <header className="col-span-full">
-      NavBar
+      <Navbar />
     </header>
     <nav className="md:row-span-[10] md:col-span-1">
       navigation
