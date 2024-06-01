fcc267c7b335aea6bfe4446a8a3b0ecaa7f382db
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-06-01 17:07:46 +0600

    feat: backend - blog get - rating added

--- backend/blog_api/serializers.py
+++ backend/blog_api/serializers.py
@@ -1,6 +1,9 @@
+from django.db.models import Avg
+
 from rest_framework import serializers
 
 from blog_api.models import Blog
+from rating_api.models import Rating
 
 from user_api.serializers import UserSerializer
 
@@ -17,6 +20,13 @@ class BlogSerializer(serializers.ModelSerializer):
 
         if instance.approved_by:
             representation['approved_by'] = UserSerializer(instance.approved_by).data
+            
+        representation['rating'] = avg_rating = Rating.objects.filter(
+            is_deleted=False,
+            blog=instance).aggregate(Avg('rating')).get('rating__avg', None)
+        
+        if representation['rating'] is not None:
+            representation['rating'] = round(representation['rating'], 2)
         
         return representation
 
