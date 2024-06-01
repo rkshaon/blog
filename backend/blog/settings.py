aa728e3a24a8dd55468e2d9f68ddf90c5a55c693
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-06-02 00:36:08 +0600

    fix: backend - cors header allow added

--- backend/blog/settings.py
+++ backend/blog/settings.py
@@ -147,6 +147,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://192.168.31.220:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://192.168.31.220:3000",
 ]
 
 AUTH_USER_MODEL = 'user_api.User'
