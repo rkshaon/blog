02ce0d0c9ca07e7578060b83c88cf88c28faf905
HussainShafayet <shafayetsec09@gmail.com>
2024-06-04 01:55:22 +0600

    (feat) Check LoggedIn for SignUp page If a user is already logged in and somehow navigates to the sign-up URL

--- frontend_react/src/components/auth/SignUp.js
+++ frontend_react/src/components/auth/SignUp.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
 
 const SignUp = () => {
+  const {isLoggedIn,login} = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate('/');
+    }
+  }, [isLoggedIn, navigate]);
+
+
   return (
     <div>SignUp</div>
   )
