ba2374c18be03b23abb40f0ff32c6cf497c1cfb8
HussainShafayet <shafayetsec09@gmail.com>
2024-06-07 23:46:50 +0600

    (Feat) Blog card design This commmit will be create a blog card

--- frontend_react/src/components/Home.js
+++ frontend_react/src/components/Home.js
@@ -1,18 +1,65 @@
 import React,{useEffect} from 'react'
 import { useAuth } from '../context/AuthContext';
 import { useCookies } from 'react-cookie';
+import Card from './Blog/Card';
 
 const Home = () => {
   const [cookies] = useCookies(['token']);
   const { isLoggedIn } = useAuth();
+  const blogList =[
+    {
+      image:"https://via.placeholder.com/400x200",
+      title:"Exploring React Components",
+      author:"Bob Martin",
+      time:"2024-06-01T12:00:00Z",
+      text:"React components are the building blocks of a React application. This article explores how to create and use them effectively...",
+      comments:"5",
+      ratings:"4.3",
+      category:"React",
+    },
+    {
+      image:"https://via.placeholder.com/400x200",
+      title:"Exploring React Components",
+      author:"Bob Martin",
+      time:"2024-06-01T12:00:00Z",
+      text:"React components are the building blocks of a React application. This article explores how to create and use them effectively...",
+      comments:"5",
+      ratings:"4.3",
+      category:"React",
+    },
+    {
+      image:"https://via.placeholder.com/400x200",
+      title:"Exploring React Components",
+      author:"Bob Martin",
+      time:"2024-06-01T12:00:00Z",
+      text:"React components are the building blocks of a React application. This article explores how to create and use them effectively...",
+      comments:"5",
+      ratings:"4.3",
+      category:"React",
+    },
+    {
+      image:"https://via.placeholder.com/400x200",
+      title:"Exploring React Components",
+      author:"Bob Martin",
+      time:"2024-06-01T12:00:00Z",
+      text:"React components are the building blocks of a React application. This article explores how to create and use them effectively...",
+      comments:"5",
+      ratings:"4.3",
+      category:"React",
+    },
+  ]
 
   //useEffect(() => {
   //}, [cookies, isLoggedIn]);
 
   return (
     <div className="p-8">
-      <h1 className="text-3xl font-bold">Home Page</h1>
-      <p>Welcome to the home page!</p>
+
+      {
+        blogList.map((item)=>(
+          <Card cardItem={item} />
+        ))
+      }
     </div>
   );
 }
