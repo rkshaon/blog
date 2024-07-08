bcc15bd46460cbd6090ce4f801919513b8f0ce3b
HussainShafayet <shafayetsec09@gmail.com>
2024-07-08 22:49:35 +0600

    not found page update

--- frontend_react/src/components/NotFound.js
+++ frontend_react/src/components/NotFound.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 Not Found</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="bg-white p-8 rounded-lg shadow-md">
+      <h2 className="text-2xl font-bold mb-4 text-center">404 - Not Found</h2>
+      <p className="text-gray-700 text-center">The page you are looking for does not exist.</p>
     </div>
+  </div>
   );
 };
 
